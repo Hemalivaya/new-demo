cdcdbc3ebe985937552931cf298b52a5c1cbc32c
hemali <himalivaaya@pwc.com>
2024-08-11 16:16:14 +0530

    Javasc and other modifications

---
+++ Trial.js
@@ -0,0 +1,15 @@
+let fullname = "hi";
+console.log(fullname);
+
+
+alert("hey")
+let a = 54;
+let b = 63;
+
+console.log("a+b",a+b);
+
+const student = {
+    student.age: "24",
+    fullName :"Rahul Kumar",
+    cgpa :8.2,
+};
